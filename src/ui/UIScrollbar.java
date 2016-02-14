4c75a9af17784765bb754241b98434ca48c1d149
Weirdbob95 <rory.soiffer@comcast.net>
2016-02-14 07:48:17 -0800

    Commit 5.1

---
+++ src/ui/UIScrollbar.java
@@ -0,0 +1,15 @@
+package ui;
+
+public class UIScrollbar {
+
+//    private static Framebuffer temp;
+//    private static
+//
+//    public Vec2 maxSize;
+//    public UIElement child;
+//
+//    public UIScrollbar(Vec2 maxSize, UIElement child) {
+//        this.maxSize = maxSize;
+//        this.child = child;
+//    }
+}
